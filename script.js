753f293b1c2538e41e138b3a54ae03ef74337197
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-07-26 23:18:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	for(let i=0;i<s1.length;i++){
+		const str1=s1.toUpperCase();
+		const str2=s2.toUpperCase();
+		if(str1.charAt(i)===str2.charAt(0)){
+			return i;
+		}
+	}
+	return -1;
 }
 
 // Please do not change the code below
